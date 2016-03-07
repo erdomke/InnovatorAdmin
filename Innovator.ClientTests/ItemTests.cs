a50089e8724cb7620481084b77ddafdc89cd593c
Eric Domke <erdomke@gmail.com>
2016-03-07 09:13:12 -0500

    Build Fixes
    Fix the build process to handle the inclusion of libgitsharp

--- Innovator.ClientTests/ItemTests.cs
+++ Innovator.ClientTests/ItemTests.cs
@@ -37,11 +37,11 @@ namespace Aras.Innovator.Tests
         aml.Property("item_number", aml.Attribute("condition", "like"), "905-1954-*")
       );
       Assert.AreEqual("<Item action=\"get\" type=\"Part\"><is_active_rev>1</is_active_rev><item_number condition=\"like\">905-1954-*</item_number></Item>", item.ToString());
-      
+
       //item = (Item)new Item().Action("get").Type("Part")
       //  .SetProperty("is_active_rev", "1")
       //  .Add(new Property("item_number", new Attribute("condition", "like"), "905-1954-*"));
-      //Assert.AreEqual("<Item action=\"get\" type=\"Part\"><is_active_rev>1</is_active_rev><item_number condition=\"like\">905-1954-*</item_number></Item>", (string)item);  
+      //Assert.AreEqual("<Item action=\"get\" type=\"Part\"><is_active_rev>1</is_active_rev><item_number condition=\"like\">905-1954-*</item_number></Item>", (string)item);
     }
 
 
@@ -79,92 +79,12 @@ namespace Aras.Innovator.Tests
         item.ToAml());
     }
 
-    public void Example()
+    [TestMethod]
+    public void VerifyItemCount()
     {
-      var aml = ElementFactory.Local;
-      var item = aml.Item(aml.Type("Part"), aml.Action("get"),
-        aml.Property("item_number", aml.Condition(Condition.Like), "905-1954-*"),
-        aml.Relationships(
-          aml.Item(aml.Type("Part BOM"), aml.Action("get"))
-        )
-      );
-
-      var query = new Command(@"<Item type='Part' action='get'>
-                                    <item_number condition='like'>@0</item_number>
-                                    <Relationships>
-                                      <Item type='Part BOM' action='get'> </Item>
-                                    </Relationships>
-                                  </Item>", "905-1954-*");
-      
-      //var conn = Factory.DirectConnection("{URL_HERE}");
-      // Login
-
-
-
-//      var result = conn.GetResult("ApplyItem", 
-//        @"<Item type='Process Change Order' action='get' select='state'>
-//            <item_number>{0}</item_number>
-//          </Item>", "yourNumber");
-//      var isValid = result.Items().Any();
-//      var state = result.AssertItem().State().Value;
-
+      var aml = @"<SOAP-ENV:Envelope xmlns:SOAP-ENV='http://schemas.xmlsoap.org/soap/envelope/'><SOAP-ENV:Body><Result><Item type='Document' typeId='B88C14B99EF449828C5D926E39EE8B89' id='9370ECBC57DD416A9465F69F1281DB74'><classification>Miscellaneous</classification><config_id keyed_name='heart-1239269 (DOC-171531)' type='Document'>9370ECBC57DD416A9465F69F1281DB74</config_id><copyright>0</copyright><created_by_id keyed_name='Eric Domke' type='User'>2D246C5838644C1C8FD34F8D2796E327</created_by_id><created_on>2016-03-04T14:34:56</created_on><current_state keyed_name='Released' type='Life Cycle State' name='Released'>C363ABDADF8D485393BB89877DBDCFD0</current_state><file_extensions>.jpg</file_extensions><generation>1</generation><has_change_pending>0</has_change_pending><has_files>1</has_files><id keyed_name='heart-1239269 (DOC-171531)' type='Document'>9370ECBC57DD416A9465F69F1281DB74</id><is_active_rev>1</is_active_rev><is_current>1</is_current><is_released>1</is_released><is_template>0</is_template><keyed_name>heart-1239269 (DOC-171531)</keyed_name><lab_controlled_document>0</lab_controlled_document><locked_by_id keyed_name='Eric Domke' type='User'>2D246C5838644C1C8FD34F8D2796E327</locked_by_id><major_rev>001</major_rev><modified_by_id keyed_name='Eric Domke' type='User'>2D246C5838644C1C8FD34F8D2796E327</modified_by_id><modified_on>2016-03-04T14:34:59</modified_on><new_version>1</new_version><not_lockable>0</not_lockable><permission_id keyed_name='New Document' type='Permission'>F0E3A6D242FC4889A9A119EEBC8EC79E</permission_id><release_date>2016-03-04T14:34:56</release_date><spec_regulation>0</spec_regulation><state>Released</state><team_id keyed_name='Owner: Public' type='Team'>2DEF50D558B44ECD9A603759D0B2D0DF</team_id><item_number>DOC-171531</item_number><name>heart-1239269</name><itemtype>B88C14B99EF449828C5D926E39EE8B89</itemtype><viewfile keyed_name='View' type='File'>F7584539F93F4F7F83A6EBF54072E6E4</viewfile></Item></Result><Message><Item id='F7584539F93F4F7F83A6EBF54072E6E4' type='File'><filename>f7584539f93f4f7f83a6ebf54072e6e4.jpg</filename></Item><event name='ids_modified' value='9370ECBC57DD416A9465F69F1281DB74|F7584539F93F4F7F83A6EBF54072E6E4|98F667F9CAB04528843D6D20738C46E6|527C835794B842A8B16E054E35B54F61' /></Message></SOAP-ENV:Body></SOAP-ENV:Envelope>";
+      var result = ElementFactory.Local.FromXml(aml);
+      Assert.AreEqual(1, result.Items().Count());
     }
-
-    //[TestMethod()]
-    //public void PerfTest()
-    //{
-    //  var builder = new StringBuilder();
-    //  builder.Append("<Result>");
-    //  for (var i = 0; i < 5000; i++ )
-    //  {
-    //    builder.Append("<Item type=\"Part\" id=\"" + i + "\"><item_number>123" + i + "</item_number><name>Test " + i + "</name><description></description><keyed_name>asdfasdfasdfsadf </keyed_name><is_current>1</is_current><permission_id>asdfsadf</permission_id></Item>");
-    //  }
-    //  builder.Append("</Result>");
-
-    //  var masterDoc = new XmlDocument();
-    //  var aml = ElementFactory.Local;
-    //  var doc = new XmlDocument(masterDoc.NameTable);
-    //  doc.LoadXml(builder.ToString());
-    //  var st = Stopwatch.StartNew();
-    //  for (var j = 0; j < 100000; j++)
-    //  {
-    //    var child = doc.DocumentElement.FirstChild.ChildNodes.OfType<XmlNode>().Where(n => n.LocalName == "permission_id");
-    //  } 
-    //  Debug.Print("Method 1 {0} ms", st.ElapsedMilliseconds);
-
-    //  st.Reset();
-    //  masterDoc = new XmlDocument();
-    //  var settings = new XmlReaderSettings();
-    //  settings.NameTable = masterDoc.NameTable;
-    //  st.Start();
-    //  for (var j = 0; j < 100000; j++)
-    //  {
-    //    var child = doc.DocumentElement.FirstChild.SelectNodes("./permission_id");
-    //  } 
-    //  Debug.Print("Method 2 {0} ms", st.ElapsedMilliseconds);
-    //  Debug.Print("hit");
-    //}
-
-    //private IEnumerable<XmlElement> GetElements(XmlDocument doc, XmlReaderSettings settings, string xml)
-    //{
-    //  using (var strReader = new System.IO.StringReader(xml))
-    //  {
-    //    using (var reader = XmlReader.Create(strReader, settings))
-    //    {
-    //      reader.Read();
-    //      while (!reader.EOF)
-    //      {
-    //        if (reader.NodeType == XmlNodeType.Element && reader.LocalName == "Item")
-    //        {
-    //          yield return (XmlElement)doc.ReadNode(reader);
-    //        }
-    //        else
-    //        {
-    //          reader.Read();
-    //        }
-    //      }
-    //    }
-    //  }
-    //}
   }
 }
--- InnovatorAdmin/AssemblyInfo.Version.cs
+++ InnovatorAdmin/AssemblyInfo.Version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("2016.03.03.2139")]
-[assembly: AssemblyFileVersion("2016.03.03.2139")]
+[assembly: AssemblyVersion("2016.03.07.0911")]
+[assembly: AssemblyFileVersion("2016.03.07.0911")]
